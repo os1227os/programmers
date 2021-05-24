82eecc852cda4e4bd6e72873a647ac211703d002
김대일 <os1227os@naver.com>
2021-05-24 22:55:50 +0900

    5.24

--- 완주하지못한선수.py
+++ 완주하지못한선수.py
@@ -1,12 +1,12 @@
 def solution(participant, completion):
-  answer = ''
+
   participant.sort()
   completion.sort()
+
   for x in range(len(completion)):
     if participant[x] != completion[x]:
-      answer = participant[x]
-    else:
-      answer = participant[-1]
-  return answer
+      return  participant[x]
+  return  participant[-1]
+  
 
 
